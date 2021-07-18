16a58d7575b55238c96e4c81260b131337825119
Samuel Adekunle <samuelbeebest@gmail.com>
2021-07-18 22:30:59 +0100

    Filtering Paths

--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
