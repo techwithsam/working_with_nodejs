949fbd19643d4aa26bab960c5785912c70ae94fc
Samuel Adekunle <samuelbeebest@gmail.com>
2021-07-19 01:38:57 +0100

    quick edit on 404 page

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "error-page.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "error-page.html"));
 });
 
 app.listen(3000);
