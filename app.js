135b729bffd13ada50954b2d1817590dddd3b20e
Samuel Adekunle <samuelbeebest@gmail.com>
2021-07-19 01:35:26 +0100

    returning a 404 page with HTML

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -8,11 +9,11 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found!!!</h1>');
+  res.sendFile(path.join(__dirname, "../", "views", "error-page.html"));
 });
 
 app.listen(3000);
