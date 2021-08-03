a5d85740cb160f25129f2e4ce6f453038b85a1f0
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-03 02:40:42 +0100

    Outputting dynamic content

--- routes/shop.js
+++ routes/shop.js
@@ -8,10 +8,8 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("shop");
-  // res.render(path.join(rootDir, "admin/shop/index"), {});
-  // console.log('shop.js', adminData.products);
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
